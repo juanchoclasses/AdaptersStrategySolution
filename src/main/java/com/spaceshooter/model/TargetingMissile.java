be821ee88cb2cec2a88805793e2c9b8e060e1148
sillyfunnypedro <sillyfunnypedro@gmail.com>
2025-04-14 13:06:39 -0700

    Add Javadoc comments to TargetingMissile class

--- src/main/java/com/spaceshooter/model/TargetingMissile.java
+++ src/main/java/com/spaceshooter/model/TargetingMissile.java
@@ -1,16 +1,34 @@
 package com.spaceshooter.model;
 
+/**
+ * A specialized missile that can track and follow a target enemy.
+ * This missile extends the basic Missile class and adds homing capabilities.
+ * It calculates the direction to its target and smoothly turns towards it while moving.
+ */
 public class TargetingMissile extends Missile {
   private static final double TURN_RATE = 0.1; // How quickly the missile can turn
   private Enemy target;
   private double currentDirection; // Current direction in radians
 
+  /**
+   * Constructs a new targeting missile at the specified position with a target enemy.
+   *
+   * @param x the initial x-coordinate of the missile
+   * @param y the initial y-coordinate of the missile
+   * @param target the enemy that the missile will track
+   */
   public TargetingMissile(int x, int y, Enemy target) {
     super(x, y, true);
     this.target = target;
     this.currentDirection = -Math.PI / 2; // Start moving upward
   }
 
+  /**
+   * Updates the missile's position and direction.
+   * If a target is set, the missile will calculate the direction to the target
+   * and smoothly turn towards it while moving. If no target is set,
+   * it behaves like a normal missile moving upward.
+   */
   @Override
   public void update() {
     if (target != null) {
