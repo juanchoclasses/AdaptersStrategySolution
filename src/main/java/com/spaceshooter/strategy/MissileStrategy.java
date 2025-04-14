c07c66c416e5eb4b5d7385039c1f64529dcf4210
sillyfunnypedro <sillyfunnypedro@gmail.com>
2025-04-14 13:08:06 -0700

    Add Javadoc comments to MissileStrategy interface

--- src/main/java/com/spaceshooter/strategy/MissileStrategy.java
+++ src/main/java/com/spaceshooter/strategy/MissileStrategy.java
@@ -2,13 +2,20 @@ package com.spaceshooter.strategy;
 
 import com.spaceshooter.model.Missile;
 
+/**
+ * Interface defining the strategy pattern for creating missiles in the Space Shooter game.
+ * Different implementations of this interface can create missiles with varying behaviors,
+ * such as basic straight-flying missiles, targeting missiles, or multiple missiles.
+ * This allows for flexible and extensible missile creation without modifying existing code.
+ */
 public interface MissileStrategy {
   /**
    * Creates a missile at the specified position.
+   * The specific behavior and type of missile created depends on the implementing class.
    *
-   * @param x The x-coordinate of the missile.
-   * @param y The y-coordinate of the missile.
-   * @return The created missile.
+   * @param x The x-coordinate where the missile will be created
+   * @param y The y-coordinate where the missile will be created
+   * @return The created missile, or null if the strategy handles missile creation internally
    */
   Missile createMissile(int x, int y);
 }
